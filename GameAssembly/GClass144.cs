8dcb349c6ebe7ba61001450976dbbaedbbbe8862
ThanhTran(KaitoKid) <thanhtran2005isme@gmail.com>
2026-09-19 19:12:19 +0700

    Reset trạng thái đặc biệt trước khi mở hành trang

--- GameAssembly/GClass144.cs
+++ GameAssembly/GClass144.cs
@@ -5951,6 +5951,11 @@ public class GClass144 : GClass131, GInterface4
 
 	public void method_109()
 	{
+		// Khi người dùng mở Hành trang/Menu thủ công, luôn thoát khỏi
+		// trạng thái animation/ghép đồ còn sót lại. Nếu sbyte_8 != -1,
+		// GClass76.method_94() sẽ bỏ qua toàn bộ panel bình thường và
+		// chỉ vẽ method_197(), dẫn tới khung trắng ở một số server.
+		GClass73.gclass76_0.sbyte_8 = -1;
 		GClass73.gclass76_0.method_40();
 		GClass73.gclass76_0.method_50();
 	}
